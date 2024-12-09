30844f085879cbce823da1275972918e69fc9300
TiagoNelvo <ti.609@hotmail.com>
2024-12-09 17:42:40 -0300

    Variaveis

---
+++ Introdução à Programação Python/Variaveis.py
@@ -0,0 +1,14 @@
+# Apresentação do curso da Descomplica - Variáveis
+
+nome_empresa = "Edugital"
+numero_funcionario = 100
+faturamento_anual = 1000000000000.00
+empresa_aberta = True
+
+print("Nome da empresa: ", nome_empresa)
+print("Número de funcionários: ", numero_funcionario)
+
+# Desafio
+
+print("Faturamento anual: ", faturamento_anual)
+print("Empresa em funcionamento? ", empresa_aberta)
