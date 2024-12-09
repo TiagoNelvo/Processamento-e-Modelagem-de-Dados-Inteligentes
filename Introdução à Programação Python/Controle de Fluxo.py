9dce5e7302a8541e99e4317db12394edef5c35a1
TiagoNelvo <ti.609@hotmail.com>
2024-12-09 17:51:06 -0300

    Controle de Fluxo

---
+++ Introdução à Programação Python/Controle de Fluxo.py
@@ -0,0 +1,17 @@
+nome_empresa = "Edugital"
+numero_funcionario = 100
+faturamento_anual = 1000000000000.00
+empresa_aberta = False
+
+if(empresa_aberta == True):
+    print("Empresa em funcionamento?: SIM")
+else:
+    print("Empresa em funcionamento?: NÃO")
+    
+# While
+
+contador = 0
+while contador < 10:
+    print("Quantidade de itens: ", contador)
+    contador += 1
+    
