45170f82665dfe24b6201347507441f747c78e5a
TiagoNelvo <ti.609@hotmail.com>
2024-12-12 18:22:04 -0300

    bugs

---
+++ 01 - Identificando erros.py
@@ -0,0 +1,21 @@
+# Simulando erros
+
+
+x = 10
+# if x = 5: (erro: o x esta recebendo o 5, o correto é == para que seja igual ao 5)
+if x == 5:
+    print('O valor de x é ', x)
+    
+lista = [1,2,3]
+#print(lista[4]) (erro: a lista compões as posições 0,1 e 2)
+print(lista[2])
+
+nums = [1,2,3,4,5]
+
+def calcular_media(a,b,c):
+    media = a + b + c / 3
+    return media
+
+# resultado = calcula_mediana(nums) (o erro esta no nome da função)
+print(resultado)
+
