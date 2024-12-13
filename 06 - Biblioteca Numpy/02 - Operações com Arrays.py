a9ba44ec6ff2fef4e573a5dd1eaf7f2ed6e98b74
TiagoNelvo <ti.609@hotmail.com>
2024-12-13 03:28:28 -0300

    Arrays

---
+++ 06 - Biblioteca Numpy/02 - Operações com Arrays.py
@@ -0,0 +1,29 @@
+import numpy as np
+
+# Array - Matrizes
+
+arr1 = np.array([1,2,3,4,5])
+
+arr1
+print(arr1)
+
+arr2 = np.array([[0,9,8,7,6,5,4],[4,5,6,7,8,9,0]])
+arr2
+print(arr2)
+
+#Soma entre duas arrays
+
+array_1 = np.array([1,1,1])
+array_2 = np.array([2,2,2])
+
+array_1
+array_2
+soma_array = array_1 + array_2
+soma_array
+
+# Subtração
+
+subratacao_array = array_2 - array_1
+subratacao_array
+
+subratacao_array[1]
