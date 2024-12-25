284b32e595cbee31f7723877d3e8fbcef261b822
TiagoNelvo <ti.609@hotmail.com>
2024-12-25 15:48:53 -0300

    Gráfico de Scatterplot

--- 09 - Introdução à Seaborn/01.py
+++ 09 - Introdução à Seaborn/01.py
@@ -55,14 +55,43 @@ sns.barplot(x='Produto',y='Vendas Fevereiro', data=df_vendas)
 
 # Gráfico de Scatterplot
 
+import pandas as pd
+import matplotlib.pyplot as plt
+
+vendas_scatter = pd.DataFrame({'Vendas':[100,150,200,250,300,350,400,450,500],
+                               'Gastos com publicicdade':[50,75,80,100,110,130,150,170,190],
+                               'Média de Preço':[20,22,23,25,27,28,30,32,33]})
+
+
+vendas_scatter
+
+#Matplot
+
+plt.scatter(x='Gastos com publicicdade',y='Média de Preço',cmap='Blues',data=vendas_scatter)
+plt.title('Relação entre gastos com publicade e vendas')
+plt.xlabel('Gastos com publicidade (em milhares de reais)')
+plt.ylabel('Vendas (em unidades)')
+plt.colorbar().set_label('Média de preço (em reais)')
+plt.show()
+
+#Seaborn
+
+sns.scatterplot(x='Gastos com publicicdade',y='Média de Preço',cmap='Blues',data=vendas_scatter)
+plt.title('Relação entre gastos com publicade e vendas')
+plt.xlabel('Gastos com publicidade (em milhares de reais)')
+plt.ylabel('Vendas (em unidades)')
+plt.show()
 
 
 
+#Exemplo Seaborn
 
-
-
-
-
-
+horas_estudads = [1,2,3,4,5,6,7,8,9,10]
+nota_obtida = [10,55,65,72,80,85,90,95,98,100]
+sns.scatterplot(x=horas_estudads, y=nota_obtida)
+plt.title('Relação entre horas estudadas e nota obtida')
+plt.xlabel('Horas estudadas')
+plt.ylabel('Nota Obtida')
+plt.show()
 
 
