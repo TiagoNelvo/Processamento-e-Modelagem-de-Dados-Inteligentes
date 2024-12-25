4fb56e6a8ed5523cb9204d5ba25025ed58112419
TiagoNelvo <ti.609@hotmail.com>
2024-12-25 14:56:14 -0300

    Introdução ao Seaborn

---
+++ 09 - Introdução à Seaborn/01.py
@@ -0,0 +1,34 @@
+# Introdução a Seaborn
+
+import seaborn as sns
+import pandas as pd
+import numpy as np
+
+notas = np.random.normal(loc=70, scale=10,size=100)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
