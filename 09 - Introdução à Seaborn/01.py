60d6dc9ac05859eb21a1709f3c7b5f52553ecb45
TiagoNelvo <ti.609@hotmail.com>
2024-12-25 15:18:52 -0300

    Gráficos de Jointplot

--- 09 - Introdução à Seaborn/01.py
+++ 09 - Introdução à Seaborn/01.py
@@ -1,5 +1,7 @@
 # Introdução a Seaborn
 
+# Gráficos de Densidade e Distribuição
+
 import seaborn as sns
 import pandas as pd
 import numpy as np
@@ -12,26 +14,26 @@ dados = pd.DataFrame({'Notas': notas})
 
 dados
 
+# Gráfico
 sns.kdeplot(data=dados['Notas'],fill=True)
 
+# Gráficos de Joinplot
 
 
+altura = np.random.normal(loc=175,scale=10,size=100)
+largura = np.random.normal(loc=70,scale=10,size=100)
 
+altura
+largura
 
+dados_joint = pd.DataFrame({'Altura': altura, 
+                            'Largura': largura})
 
+dados_joint
 
+# Gráfico
 
-
-
-
-
-
-
-
-
-
-
-
+sns.jointplot(x='Altura', y='Largura', data=dados_joint, kind='kde',color='blue')
 
 
 
