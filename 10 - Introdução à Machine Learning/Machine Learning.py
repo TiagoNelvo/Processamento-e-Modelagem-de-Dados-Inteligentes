e4347c7efa2510e34d6221c1ab486e729f0a9253
TiagoNelvo <ti.609@hotmail.com>
2024-12-25 16:07:54 -0300

    Introdução ao Aprendizado de Máquina

---
+++ 10 - Introdução à Machine Learning/Machine Learning.py
@@ -0,0 +1,47 @@
+# 01 - Introdução ao Aprendizado de Máquina
+
+# Aprendizado de Máquina e IA
+
+import numpy as np
+from sklearn.linear_model import LogisticRegression
+
+altura = [1.60,1.75,1.68,1.80,1.62,1.65,1.90,1.85]
+peso = [55,70,60,75,58,62,85,80]
+sexo = ['Feminino','Masculino','Feminino', 'Masculino', 'Feminino','Feminino','Masculino','Masculino']
+
+modelo = LogisticRegression()
+
+X = np.array(list(zip(altura,peso)))
+y = np.array(sexo)
+
+modelo.fit(X,y)
+
+nova_amostra = np.array([[1.50,54]])
+decisao = modelo.predict(nova_amostra)
+
+if decisao[0] == 'Feminino':
+    print('Provavelmente é uma mulher')
+else:
+    print('Provavelemente é um homem')
+
+
+
+
+
+
+# 02 - Aprendizado Supervisionado e Não-Supervisionado
+
+
+
+# 03 - Regressão Linear e Logística
+
+
+
+# 04 - K-NN
+
+
+
+# 05 - Árvores de Decisão
+
+
+
